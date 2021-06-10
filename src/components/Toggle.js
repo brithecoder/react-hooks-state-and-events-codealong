6be705ff4b0e4d2fac170aff5da10b5afbc3abf2
Brianna Gordon <brigordon574@gmail.com>
2021-06-10 16:52:19 -0700

    commit tree f7b3c5a3ccc02ad53082c6aeeafc969aabdd24d7

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  
+  const [toggler,setToggler] = useState(false);
+  
+  function handleClick(){
+   setToggler ((toggler) => !toggler);
+  }
+  const color = toggler? "red":"white";
+  return <button onClick={handleClick} style={{background :color}}> {toggler ? "ON":"OFF"}</button>;
 }
 
 export default Toggle;
